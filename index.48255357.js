bfcd5e9ca9cf5ea076e739b7a700ecebc3400710
Maksym <makstarasovv1@gmail.com>
2025-01-15 19:30:25 +0200

    make build

---
+++ index.48255357.js
@@ -0,0 +1,2 @@
+var e,t,c,l,o,i;e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,o=t.offsetWidth,i=t.offsetHeight,t.style.top="".concat((l-i)/2,"px"),t.style.left="".concat((c-o)/2,"px");
+//# sourceMappingURL=index.48255357.js.map
